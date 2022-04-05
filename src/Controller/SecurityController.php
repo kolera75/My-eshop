4232131bb892f5aa4be7734ff70df282c5dd5cf7
kolera75 <amine.sebki@gmail.com>
2022-04-05 14:57:57 +0200

    Create Register action + make:auth ok

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,6 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use DateTime;
 
 class SecurityController extends AbstractController
 {
@@ -44,17 +45,45 @@ class SecurityController extends AbstractController
      * 
      */
 
-    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response 
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
 
         $user = new User();
 
         $form = $this->createForm(RegisterType::class, $user)->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            //set des propriétés qui ne sont pas dans le formulaire
+
+            $user->setCreatedAt(new DateTime());
+            $user->setUpdatedAt(new DateTime());
+            $user->setRoles(['ROLE_USER']);
+
+            
+            // Nous devons setter manuellement le hash du paswword grace au $passwordHasher et sa méthode hashPassword()
+            #  => cette méthode prend 2 param : $user, $plainPassword
+
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    $form->get('password')->getData()
+
+                )
+            );
+
+                //On envoie tout en BDD grâce à notre $entityManager
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre inscription est validée. Connectez-vous à présent !');
+            return $this->redirectToRoute('app_login');
+        }
+
+
+
         return $this->render('security/form_register.html.twig', [
             'form' => $form->createView(),
         ]);
-
-
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,13 +4,16 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegisterType extends AbstractType
 {
@@ -19,15 +22,77 @@ class RegisterType extends AbstractType
         $builder
             ->add('email', EmailType::class, [
                 'label' => 'Email',
+                'constraints' => [
+
+                    new NotBlank([
+                        'message' => 'Ce champ ne peut être vide',
+                    ]),
+
+                    new Length([
+                        'max' => 180,
+                        'maxMessage' => 'Votre email ne peut dépasser {{ limit }} caractères',
+                    ]),
+
+                    new Email([
+                        'message' => 'Votre email n\'est pas au bon format: ex. mail@example.com'
+                    ]),
+                ],
             ])
             ->add('password', PasswordType::class, [
                 'label' => ' Mot de passe',
+                'constraints' => [
+
+                    new NotBlank([
+                        'message' => 'Ce champ ne peut être vide',
+                    ]),
+
+                    new Length([
+                        'min' => 4,
+                        'max' => 255,
+                        'minMessage' => 'Votre mot de passe doit avoir au minimum {{ limit }} caractères',
+                        'maxMessage' => 'Votre mot de passe ne peut dépasser {{ limit }} caractères',
+
+
+
+
+                    ])
+                ],
             ])
             ->add('firstname', TextType::class, [
                 'label' => 'Prénom',
+                'constraints' => [
+
+                    new NotBlank([
+                        'message' => 'Ce champ ne peut être vide',
+                    ]),
+
+                    new Length([
+
+                        'max' => 50,
+                        'min' => 2,
+                        'maxMessage' => 'Votre prénom ne peut dépasser {{ limit }} caractères',
+                        'minMessage' => 'Votre prénom doit avoir au minimum {{ limit }} caractères',
+                    ])
+
+                ]
             ])
             ->add('lastname', TextType::class, [
-                'label' => 'Nom'
+                'label' => 'Nom',
+                'constraints' => [
+
+                    new NotBlank([
+                        'message' => 'Ce champ ne peut être vide',
+                    ]),
+
+                    new Length([
+
+                        'max' => 50,
+                        'min' => 2,
+                        'maxMessage' => 'Votre nom ne peut dépasser {{ limit }} caractères',
+                        'minMessage' => 'Votre nom doit avoir au minimum {{ limit }} caractères',
+                    ])
+
+                ]
             ])
             ->add('gender', ChoiceType::class, [
 
@@ -41,20 +106,29 @@ class RegisterType extends AbstractType
 
                 'attr' => [
 
-                    'class' => '',
+                    'class' => 'd-flex gap-3',
                 ],
+
+                'constraints' => [
+
+                    new NotBlank([
+                        'message' => 'Veuillez choisir un genre',
+                    ]),
+
+                ]
 
             ])
 
             ->add('submit', SubmitType::class, [
-                'label' => 'Valider', 
+                'label' => 'Valider',
                 // Cette option permet de désactiver le validator HTML (front), comme on a fait en twig (voir ci-dessous)
                 # => form_start(form,  {'attr': {'novalidate': novalidate}})
 
                 'validate' => false,
-            ])
-          
-        ;
+                'attr' => [
+                    'class' => 'd-block mx-auto col-2 btn btn-warning'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
